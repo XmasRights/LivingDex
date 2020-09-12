fa3324017bcdbfe706af6357da839ecd120550eb
Christopher Fonseka <chris.fonseka@googlemail.com>
2020-09-12 23:32:26 +0100

    Living Dex Xcode Project is Go!

---
+++ Living Dex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Living Dex
+//
+//  Created by Christopher Fonseka on 12/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Living Dex/Living_DexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Living_DexApp.swift
+//  Living Dex
+//
+//  Created by Christopher Fonseka on 12/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Living_DexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
